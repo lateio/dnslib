c88edbe4c0cc7e4eb34ef645b89d90fd6de17c68
Lauri Moisio <l@arv.io>
2019-04-09 20:07:33 +0300

    Support generic form in dnsrr:from_to/3
    src/dnsrr.erl:
        - Support generic form in dnsrr:from_to/3
        - Character case ignored when handling
          'masterfile_token' and 'masterfile_token_generic'
          tranformations
    doc: Document changes
    test: Update tests

--- src/dnsrr.erl
+++ src/dnsrr.erl
@@ -255,20 +255,51 @@ build_map_form([{Key, Module}|Rest], Acc) ->
     build_map_form(Rest, [{map_field_assoc, 0, {Type, 0, Key}, {atom, 0, Module}}|Acc]).
 
 
+-spec from_to(
+    Value :: atom() | 0..16#FFFF | string(),
+    From :: 'value' | 'module' | 'atom' | 'masterfile_token',
+    To   :: 'value' | 'module' | 'atom' | 'masterfile_token' | 'masterfile_token_generic'
+) -> atom() | 0..16#FFFF | string().
 from_to(Value, value, module) ->
     maps:get(Value, dnsrr_types:value(), Value);
+from_to(Value, value, masterfile_token) when Value >= 0, Value =< 16#FFFF ->
+    case maps:get(Value, dnsrr_types:value(), Value) of
+        Value -> from_to(Value, value, masterfile_token_generic);
+        Module -> Module:masterfile_token()
+    end;
+from_to(Value, value, masterfile_token_generic) when Value >= 0, Value =< 16#FFFF ->
+    "type" ++ integer_to_list(Value);
 from_to(Value, atom, module) ->
     maps:get(Value, dnsrr_types:atom(), Value);
-from_to(Value, masterfile_token, module) ->
-    maps:get(Value, dnsrr_types:masterfile_token(), Value);
+from_to(Value0, masterfile_token, To) ->
+    case string:(?LOWER)(Value0) of
+        [$t, $y, $p, $e|Int] ->
+            try list_to_integer(Int) of
+                Value when To =:= value, Value >= 0, Value =< 16#FFFF -> Value;
+                Value when Value >= 0, Value =< 16#FFFF ->
+                    case from_to(Value, value, To) of
+                        Value -> Value0;
+                        ToValue -> ToValue
+                    end;
+                _ -> Value0
+            catch
+                error:badarg -> Value0
+            end;
+        Value ->
+            case maps:get(Value, dnsrr_types:masterfile_token(), Value0) of
+                Value0 -> Value0;
+                Module when To =:= module -> Module;
+                Module -> from_to(Module, module, To)
+            end
+    end;
 from_to(Module, module, value) ->
     Module:value();
 from_to(Module, module, atom) ->
     Module:atom();
 from_to(Module, module, masterfile_token) ->
     Module:masterfile_token();
-    % However, with TYPE100 -syntax, every type has a masterfile token, even
-    % if it doesn't export one...
+from_to(Module, module, masterfile_token_generic) ->
+    from_to(Module:value(), value, masterfile_token_generic);
 from_to(Value, From, To) when From =/= To ->
     % If either From or To are not allowed, function_clause exception will result
     case from_to(Value, From, module) of

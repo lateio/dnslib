3fb5dd83fb02f3b6522535de0f4f9394c231d01e
Lauri Moisio <l@arv.io>
2019-04-09 20:12:31 +0300

    Support generic form in dnsclass:from_to/3
    src/dnsclass.erl:
        - Support generic form in dnsrr:from_to/3
        - Character case ignored when handling
          'masterfile_token' and 'masterfile_token_generic'
          tranformations
    doc: Document changes
    test: Add tests for dnsclass module

--- src/dnsclass.erl
+++ src/dnsclass.erl
@@ -26,6 +26,8 @@
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
+-include("include/pre_otp20_string_macro.hrl").
+
 -callback atom() -> atom().
 -callback value() -> 0..16#FFFF.
 -callback masterfile_token() -> string().
@@ -63,20 +65,51 @@ builtin_modules_sanity_test() ->
 -endif.
 
 
+-spec from_to(
+    Value :: atom() | 0..16#FFFF | string(),
+    From :: 'value' | 'module' | 'atom' | 'masterfile_token',
+    To   :: 'value' | 'module' | 'atom' | 'masterfile_token' | 'masterfile_token_generic'
+) -> atom() | 0..16#FFFF | string().
 from_to(Value, value, module) ->
     maps:get(Value, dnsclass_classes:value(), Value);
+from_to(Value, value, masterfile_token) when Value >= 0, Value =< 16#FFFF ->
+    case maps:get(Value, dnsclass_classes:value(), Value) of
+        Value -> from_to(Value, value, masterfile_token_generic);
+        Module -> Module:masterfile_token()
+    end;
+from_to(Value, value, masterfile_token_generic) when Value >= 0, Value =< 16#FFFF ->
+    "class" ++ integer_to_list(Value);
 from_to(Value, atom, module) ->
     maps:get(Value, dnsclass_classes:atom(), Value);
-from_to(Value, masterfile_token, module) ->
-    maps:get(Value, dnsclass_classes:masterfile_token(), Value);
+from_to(Value0, masterfile_token, To) ->
+    case string:(?LOWER)(Value0) of
+        [$c, $l, $a, $s, $s|Int] ->
+            try list_to_integer(Int) of
+                Value when To =:= value, Value >= 0, Value =< 16#FFFF -> Value;
+                Value when Value >= 0, Value =< 16#FFFF ->
+                    case from_to(Value, value, To) of
+                        Value -> Value0;
+                        ToValue -> ToValue
+                    end;
+                _ -> Value0
+            catch
+                error:badarg -> Value0
+            end;
+        Value ->
+            case maps:get(Value, dnsclass_classes:masterfile_token(), Value0) of
+                Value0 -> Value0;
+                Module when To =:= module -> Module;
+                Module -> from_to(Module, module, To)
+            end
+    end;
 from_to(Module, module, value) ->
     Module:value();
 from_to(Module, module, atom) ->
     Module:atom();
 from_to(Module, module, masterfile_token) ->
     Module:masterfile_token();
-    % However, with CLASS100 -syntax, every class has a masterfile token, even
-    % if it doesn't export one...
+from_to(Module, module, masterfile_token_generic) ->
+    from_to(Module:value(), value, masterfile_token_generic);
 from_to(Value, From, To) when From =/= To ->
     % If either From or To are not allowed, function_clause exception will result
     case from_to(Value, From, module) of
